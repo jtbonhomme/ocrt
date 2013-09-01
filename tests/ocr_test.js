9e1cc54c77d3f4bafc1ab61f7da3ea6767f2cac2
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2013-09-01 06:01:27 +0200

    Added first mocha tests

--- tests/ocr_test.js
+++ tests/ocr_test.js
@@ -1,19 +1,73 @@
-var ocrt = require('../lib/ocrt');
+var ocrt          = require('../lib/ocrt');
+var should        = require('should');
 
-var image    = "./picture.png";
-var language = "fra";
-var refStr   = "Retour sur l'apparition de nouveaux géants essentiellement découverts en Afrique, comme le spinosaure, l'argentinosaure ou encore le Predator X.";
-var crop = {
+var image         = "./picture.png";
+var croppedImage  = "./cropped_picture.png";
+var language      = "fra";
+var refStr        = "Retour sur l'apparition de nouveaux géants essentiellement découverts en Afrique, comme le spinosaure, l'argentinosaure ou encore le Predator X.";
+var refStrX       = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+var crop          = {
 	x: 10,
 	y: 10,
 	w: 10,
 	h: 10
 };
 
-ocrt.process(image, refStr, crop, function(err, result) {
-    if(err) {
-        console.error(err);
-    } else {
-        console.log("Comparison result : " + result);
-    }
+describe('ocrt.process', function() {
+    describe('with no arguments', function() {
+        it('returns -1', function() {
+            var result = ocrt.process();
+            result.should.eql(-1);
+        });
+    });
+
+    describe('with a null image, a reference string not included in the image and no callback', function() {
+        it('returns -1', function() {
+            var result = ocrt.process(null, refStrX);
+            result.should.eql(-1);
+        });
+    });
+
+    describe('with an image, a reference string not included in the image and no callback', function() {
+        it('returns -1', function() {
+            var result = ocrt.process("image.png", null);
+            result.should.eql(-1);
+        });
+    });
+
+    describe('with an image not present, a reference string not included in the image and no callback', function() {
+        it('returns -1', function() {
+            var result = ocrt.process("dummy_image.png", refStrX);
+            result.should.eql(-1);
+        });
+    });
+
+    describe('with a cropped image, a reference string not included in the image and no callback', function() {
+        it('returns 0', function() {
+            var result = ocrt.process(croppedImage, refStrX);
+            result.should.eql(0);
+        });
+    });
+
+    describe('with a cropped image, a reference string included in the image and no callback', function() {
+        it('returns 1', function() {
+            var result = ocrt.process(croppedImage, refStr);
+            result.should.eql(1);
+        });
+    });
+
+    describe('with an image to be cropped, a reference string not included in the image and no callback', function() {
+        it('returns 0', function() {
+            var result = ocrt.process(image, refStrX, crop);
+            result.should.eql(0);
+        });
+    });
+
+    describe('with a cropped image, a reference string included in the image and no callback', function() {
+        it('returns 1', function() {
+            var result = ocrt.process(image, refStr, crop);
+            result.should.eql(1);
+        });
+    });
+
 });

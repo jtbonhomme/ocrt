3464a3353197a1741214e780ac6b7bb4583b39f7
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2013-09-01 20:37:29 +0200

    Bump version 0.0.1

--- tests/ocr_test.js
+++ tests/ocr_test.js
@@ -8,13 +8,14 @@ var language      = "fra";
 var refStr        = "Retour sur l'apparition de nouveaux géants essentiellement découverts en Afrique, comme le spinosaure, l'argentinosaure ou encore le Predator X.";
 var refStrX       = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 var crop          = {
-	x: 10,
-	y: 10,
-	w: 10,
-	h: 10
+	x: 950,
+	y: 1110,
+	w: 2000,
+	l: 350
 };
 
 describe('ocrt.process', function() {
+    this.timeout(20000);
     describe('with no arguments', function() {
         it('returns -1', function() {
             ocrt.process(null, null, null, null, function(err, result){
@@ -40,7 +41,6 @@ describe('ocrt.process', function() {
     });
 
     describe('with a full image', function() {
-        this.timeout(20000);
         it('returns 0', function(done) {
             ocrt.process(image, refStrX, null, null, function(err, result){
                 result.should.eql(0);
@@ -76,14 +76,13 @@ describe('ocrt.process', function() {
         });
     });
 
-/*
-    describe('with an image to be cropped, a reference string not included in the image and no callback', function() {
-        it('returns 0', function() {
-            var result = ocrt.process(image, refStrX, crop);
-            result.should.eql(0);
+    describe('with an image to be cropped, a reference string included in the image and a "fra" language', function() {
+        it('returns 1', function(done) {
+            ocrt.process(image, refStr, crop, "fra", function(err, result){
+                result.should.eql(1);
+                done();
+            });
         });
     });
-
-*/
 
 });
